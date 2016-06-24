c272ffb72311eed325c1472cd695e0e46f513248
Joao Paulo Jung Ramia <joao.ramia@gmail.com>
2016-06-24 12:28:50 -0400

    second attempt to modularize

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ gulp.task('watch', function () {
 });
 
 gulp.task('scripts', function() {
-  return gulp.src(['./public/js/myScript.js', './public/js/keyboard.js', './public/js/mouse.js', './public/js/stickfigure.constructor.js', './public/js/select.functionality.js'])
+  return gulp.src(['./browser/js/*.js', '/browser/**/*.js'])
     .pipe(concat('main.js'))
     .pipe(gulp.dest('./public/js/'));
 });

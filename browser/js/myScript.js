ae3a1220e8175b30ed980471dfc46f28de609ad2
Joao Paulo Jung Ramia <joao.ramia@gmail.com>
2016-06-27 11:45:15 -0400

    Update myScript.js

--- browser/js/myScript.js
+++ browser/js/myScript.js
@@ -12,112 +12,103 @@ var newFigure = {
   limb: undefined
 }
 
-function checkIfFigureValidAndRemoveIfNot(){
-  var invalid = false;
-  if (newFigure.limbs.length > 0) {
-    invalid = true;
-    for (var i = 0; i < newFigure.limbs.length; i++) {
-      newFigure.limbs[i].remove();
-    }
-    newFigure.limbs = [];
-  }
-  //later, replace this with an error that the user can see
-  if (invalid) console.log("invalid figure! deleting!");
-  else console.log("valid figure!");
-  
-}
-
 $("#draw-stick-figure-button").click(function(){
   $(this).toggleClass("selected");
-  //remove figure from canvas if not valid -- only valid if made with 4 strokes
-  if (drawing) {
-    checkIfFigureValidAndRemoveIfNot();
-  }
   drawing = !drawing;
   console.log("drawing? : ", drawing);
 })
 
+function finishDrawing (){
+  drawnFigure = new Group(newFigure.limbs);
+  var newFigureColor = {
+    hue: Math.random() * 360,
+    saturation: 1,
+    brightness: 1
+  };
+  var figure = new StickFigure(drawnFigure, newFigureColor);
+  figure.representation.strokeColor = newFigureColor;
+  stickFiguresOnCanvas.push(figure);
+  newFigure = {
+    limb: undefined,
+    limbs: []
+  }
+  $("#draw-stick-figure-button").removeClass("selected");
+  drawing = false;
+}
+
+function cancelDrawing (){
+  for (var i = 0; i < newFigure.limbs.length; i++) {
+    newFigure.limbs[i].remove();
+  }
+  newFigure.limbs = [];
+  $("#draw-stick-figure-button").removeClass("selected");
+}
+
 function onKeyDown(event) {
-  $("#draw-stick-figure-button").toggleClass("selected");
-  if (event.key === 'd') {
-    if (drawing === true) {
-      checkIfFigureValidAndRemoveIfNot();
-      return drawing = false;
-    } 
+  if (event.key === 'd' && newFigure.limbs.length > 0) {
+    finishDrawing();
+  }
+  else if (event.key === 'd' && newFigure.limbs.length === 0){
+    $("#draw-stick-figure-button").toggleClass("selected");
+    drawing = !drawing;
+  }
+  else if (event.key === 'c' && drawing){
+    cancelDrawing();
     drawing = !drawing;
   }
 }
 
 function onMouseDown (event) {
-  if (drawing && newFigure.limbs.length < 4) {
-      newFigure.limb = new Path();
-      newFigure.limb.strokeColor = "black";
-      newFigure.limb.strokeWidth = 6;
-      newFigure.limb.strokeJoin = 'miter'; 
-  } else {
+  if (event.event.button === 0 && drawing){
+    newFigure.limb = new Path();
+    newFigure.limb.strokeColor = "black";
+    newFigure.limb.strokeJoin = 'miter';
+    newFigure.limb.strokeWidth = 6;
+  }
+  else if (event.event.button === 0 && !drawing){
     initialSelectPoint = event.point;
     newSelection = new Shape.Rectangle(event.point, event.point);
+  }
+  else if (event.event.button === 2){
+    initialSelectPoint = undefined;
+    event.preventDefault(); //maybe not necessary
+    currentlySelectedFigures.forEach(function(figure){
+      figure.destination = event.point;
+    })
   }
 }
 
 function onMouseDrag (event) {
-  if (drawing && newFigure.limbs.length < 4) {
+  if (drawing && event.event.button !== 2) {
     newFigure.limb.add(event.point);
-  } else {
+  } 
+  else if (event.event.button !== 2 && initialSelectPoint){
+    if (newSelection) {
+      newSelection.remove();
+    }
     newFigure.limb = null;
-    if (newSelection) newSelection.remove();
     newSelection = new Shape.Rectangle(initialSelectPoint, event.point);
     newSelection.fillColor = 'rgb(233, 233, 255, 0.5)';
-    newSelection.selected = true;
   }
 }
 
 function onMouseUp (event) {
   if (drawing) {
-    if (newFigure.limbs.length < 4) {
-      newFigure.limb.smooth();
-      newFigure.limb.strokeColor = "black";
-      newFigure.limbs.push(newFigure.limb);
-      console.log("LIMB", newFigure.limb);
-      console.log("LIMBS", newFigure.limbs);
-
-      if (newFigure.limbs.length === 4) {
-        drawnFigure = new Group(newFigure.limbs);
-        var newFigureColor = {
-          hue: Math.random() * 360,
-          saturation: 1,
-          brightness: 1
-        };
-        var figure = new StickFigure(drawnFigure, newFigureColor);
-
-        figure.representation.strokeColor = newFigureColor;
-        destination = figure.representation.position;
-        stickFiguresOnCanvas.push(figure);
-        //reset the new figure object
-        newFigure = {
-          limb: undefined,
-          limbs: []
-        }
-        $("#draw-stick-figure-button").removeClass("selected");
-        drawing = false;
-      }
-    } 
-  } else if (newSelection) {
+    newFigure.limb.smooth();
+    newFigure.limb.strokeColor = "black";
+    newFigure.limbs.push(newFigure.limb);
+  } else if (newSelection && event.event.button === 0) {
     showIntersections();
     newSelection.remove();
   }
-
 }
 
 function onFrame (event) {
-    var vector = destination - newFigure.position;
-    newFigure.position += vector / 30;
-    if (newFigure.position === destination) {
-      figureSelected = false;
-      newFigure.strokeColor = {
-        hue: Math.random() * 360,
-        saturation: 1,
-        brightness: 1
-      }
-    }
+  var movingFigures = currentlySelectedFigures;
+  movingFigures.forEach(function(figure){
+    var vector = figure.destination - figure.representation.position;
+    findBodyParts(figure, event);
+    // walk(figure, event);
+    figure.representation.position += vector / 80;
+  })
 }
